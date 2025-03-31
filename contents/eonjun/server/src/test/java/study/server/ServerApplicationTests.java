5e56056b718608b2f9a639e5ae8c974ed42ce52d
frozzun <eonjun0783@naver.com>
2025-03-31 19:47:13 +0900

    springboot initial commit

---
+++ contents/eonjun/server/src/test/java/study/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
