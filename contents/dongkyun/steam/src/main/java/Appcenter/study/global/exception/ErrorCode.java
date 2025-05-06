a772f5a10346884b049ee1dde59a8aba011865c2
d6nggyun <d6nggyun@gmail.com>
2025-05-06 20:54:42 +0900

    [Refactor] 유효성 검사 예외 처리 추가 - #54

--- contents/dongkyun/steam/src/main/java/Appcenter/study/global/exception/ErrorCode.java
+++ contents/dongkyun/steam/src/main/java/Appcenter/study/global/exception/ErrorCode.java
@@ -18,6 +18,9 @@ public enum ErrorCode {
     JWT_ENTRY_POINT(HttpStatus.UNAUTHORIZED, 401, "[Jwt] 인증되지 않은 사용자입니다."),
     JWT_ACCESS_DENIED(HttpStatus.FORBIDDEN, 403, "[Jwt] 리소스에 접근할 권한이 없습니다."),
 
+    // Validation
+    VALIDATION_FAILED(HttpStatus.BAD_GATEWAY, 400, "요청한 값이 올바르지 않습니다."),
+
     // Member
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "맴버를 찾을 수 없습니다."),
     LOGIN_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 이메일입니다."),
--- contents/dongkyun/steam/src/main/java/Appcenter/study/global/exception/ErrorResponseEntity.java
+++ contents/dongkyun/steam/src/main/java/Appcenter/study/global/exception/ErrorResponseEntity.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 @Data
 @Builder
 public class ErrorResponseEntity {
@@ -11,6 +13,7 @@ public class ErrorResponseEntity {
     private Integer code;
     private String name;
     private String message;
+    private List<String> errors;
 
     public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode errorCode) {
         return ResponseEntity
@@ -19,6 +22,18 @@ public class ErrorResponseEntity {
                         .code(errorCode.getCode())
                         .name(errorCode.name())
                         .message(errorCode.getMessage())
+                        .build()
+                );
+    }
+
+    public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode errorCode, String message, List<String> errors) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponseEntity.builder()
+                        .code(errorCode.getCode())
+                        .name(errorCode.name())
+                        .message(message)
+                        .errors(errors)
                         .build()
                 );
     }
